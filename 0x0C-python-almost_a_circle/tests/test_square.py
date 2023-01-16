9277ad160a898ec8f0d08ee2827f6990e653ad25
Nanagyamera <nanagyamera3@gmail.com>
2023-01-16 10:37:51 -0800

    Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,20 @@
+# #!/usr/bin/python3
+# """Unittest square.
+# Test cases for the Square class.
+# Each test has the number of the task,
+# and the number of the test for that task
+# (i.e 'test_17_0' for the first test of task 17)
+# """
+
+# import unittest
+# import io
+# import contextlib
+# from models.base import Base
+# from models.rectangle import Rectangle
+# from models.square import Square
+
+
+# class TestSquare(unittest.TestCase):
+#     """Test class for Square class."""
+
+#     def setUp(self):
