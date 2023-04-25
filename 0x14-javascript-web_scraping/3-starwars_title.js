098ead4ff572002a6518db2a397d8d4c6eafc7a5
Nanagyamera <nanagyamera3@gmail.com>
2023-04-25 15:10:41 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
-
-let episode = parseInt(process.argv[2]);
-let url = 'http://swapi.co/api/films/' + episode;
-let data;
-
+let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error != null) {
-    console.log(error);
-  } else {
-    data = JSON.parse(body);
-    console.log(data['title']);
-  }
+  console.log(error || JSON.parse(body).title);
 });
